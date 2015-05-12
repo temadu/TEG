e47ec302fd29c8991456b042fe8b7339c4146953
LucasMaspero <masperolucas@gmail.com>
2015-05-12 14:23:11 -0300

    Add GameBox Class

---
+++ src/backend/GameBox.java
@@ -0,0 +1,37 @@
+package backend;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class GameBox implements Serializable {
+
+	// Serializable
+	private static final long serialVersionUID = 1L;
+	
+	// Singleton
+	private static GameBox game;
+
+	// private Board board;
+	// private List<Player> players;
+	
+	private GameBox() {
+		
+		// board = new Board();
+		// players = new ArrayList<Player>();
+		
+	}
+	
+	public static GameBox getInstance() {
+		if(game == null) {
+			game = new GameBox();
+		}
+		return game;
+	}
+	
+	/* public addPlayer(Player p) {
+		
+		players.add(p);
+		
+	} */
+	
+}
