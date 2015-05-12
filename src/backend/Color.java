723814fcd21c52a9d8dcd9444e8d2697339cf5e6
temadu <teofiloduffau@gmail.com>
2015-05-12 14:18:53 -0300

    add Player class & Color enum

---
+++ src/backend/Color.java
@@ -0,0 +1,10 @@
+package backend;
+
+public enum Color {
+	BLACK,
+	WHITE,
+	RED,
+	BLUE,
+	GREEN,
+	YELLOW;
+}
---
+++ src/backend/Player.java
@@ -0,0 +1,19 @@
+package backend;
+
+import java.util.List;
+import java.util.Set;
+
+public class Player {
+	
+	private String name;
+	private Color color;
+	
+	//private List<Card> cards;       needs implementation;
+	//private Objective objective
+	
+	private int totalSoldiers;
+	private int leftOverSoldiers;
+	
+	private Set<Country> countries;
+
+}
