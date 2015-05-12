86ca8a7b24cc7ff402c5b79563fec2057117d845
temadu <teofiloduffau@gmail.com>
2015-05-12 14:42:14 -0300

    add Board class & Continent enum

---
+++ src/backend/Board.java
@@ -0,0 +1,10 @@
+package backend;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Board {
+	
+	private HashMap<Continent, ArrayList<Country>> continents;
+
+}
---
+++ src/backend/Continent.java
@@ -0,0 +1,11 @@
+package backend;
+
+public enum Continent {
+	SOUTHAMERICA,
+	NORTHAMERICA,
+	EUROPE,
+	ASIA,
+	AFRICA,
+	OCEANIA;
+
+}
