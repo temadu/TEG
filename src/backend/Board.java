58279dae9e485ec086d9c172e253dcf95a006e13
temadu <teofiloduffau@gmail.com>
2015-05-13 00:20:30 -0300

    modify Board & add Nexus enum;

--- src/backend/Board.java
+++ src/backend/Board.java
@@ -2,9 +2,19 @@ package backend;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Board {
 	
 	private HashMap<Continent, ArrayList<Country>> continents;
+	
+	private List<Country> countries;
+	private Nexus[][] adjacentMatrix;
+	
+	public Board() {
+		continents = new HashMap<Continent, ArrayList<Country>>();
+		countries = new ArrayList<Country>();
+		//adjacentMatrix
+	}
 
 }
---
+++ src/backend/Nexus.java
@@ -0,0 +1,7 @@
+package backend;
+
+public enum Nexus {
+	LAND,
+	WATER,
+	NONE;
+}
