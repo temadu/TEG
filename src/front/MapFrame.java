20d01f271c830bd5a1214c38f932127d79ee832e
Lautarongc <lautaro.ngc@gmail.com>
2015-05-14 20:37:45 -0300

    Update MapFrame

--- src/front/MapFrame.java
+++ src/front/MapFrame.java
@@ -1,5 +1,7 @@
 package front;
 
+import front.BackgroundPanel;
+
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -8,14 +10,18 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class MapFrame extends JInternalFrame {
 
     private static final int xOffset = 30, yOffset = 30;
     private BufferedImage image;
     private JDesktopPane desktop;
+    private JPanel contentPane;
     
     public MapFrame() {
         super("Map", 
@@ -33,12 +39,23 @@ public class MapFrame extends JInternalFrame {
         //Set the window's location.
         setLocation(xOffset, yOffset);
         
+    	
         try {                
             image = ImageIO.read(new File("assets/fantasymap.jpg"));
          } catch (IOException ex) { }
-        JLabel picLabel = new JLabel(new ImageIcon(image));
-        add(picLabel);
-        
+//       JLabel picLabel = new JLabel(new ImageIcon(image));
+//        add(picLabel);
+    
+	
+		final BackgroundPanel background = new BackgroundPanel(image,
+				BackgroundPanel.SCALED, 0.50f, 0.5f);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1024, 680);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(background);
+		setResizable(false);
+		background.setLayout(null);
     }
 	
 }
