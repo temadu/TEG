d79d3c6086a1cc7b241cdb376b2b645e73410d22
LucasMaspero <masperolucas@gmail.com>
2015-06-18 16:14:06 -0300

    Beautiful code & english objectives.

--- src/objectives/Objective.java
+++ src/objectives/Objective.java
@@ -1,29 +1,26 @@
 package objectives;
 
 import assets.Player;
+
 /**
  * Abstract class that represents the general parts of an objective.
  * To be extended to add the specific parts of the objective.
  * Uses the Template Method Pattern Strategy for that.
  */
+
 public abstract class Objective {
 	
 	private static final int COUNTRIESTOCONQUER = 26;
+	
 	private Player owner;
 	private String description;
 	
-	/**
-	 * Checks if the general objective or the specific objective has been fulfilled.
-	 * @return
-	 */
+	// Checks if the general objective or the specific objective has been fulfilled.
 	public boolean checkObjective(){
 		return (((owner.countriesNumber() >= COUNTRIESTOCONQUER) || checkSpecificObjective()) ? true : false);
 	}
 	
-	/**
-	 * The speciffic objective to be extended.
-	 * @return
-	 */
+	// The speciffic objective to be extended.
 	public abstract boolean checkSpecificObjective();
 
 	public Player getOwner() {
--- src/objectives/TripleContinentObjective.java
+++ src/objectives/TripleContinentObjective.java
@@ -1,17 +1,18 @@
 package objectives;
 
-
 public class TripleContinentObjective extends ConquerorObjective {
 
 	public TripleContinentObjective() {
-		super.setDescription("Conquistar Gilacia, Arlas y Algos");
+		super.setDescription("Conquer Gilacia, Arlas and Algos.");
 	}
 	
 	@Override
 	public boolean checkSpecificObjective() {
+		
 		return ((getOwner().hasContinent(getGilacia())) &&
 				(getOwner().hasContinent(getArlas())) &&
 				(getOwner().hasContinent(getAlgos())));
+	
 	}
 
 }
